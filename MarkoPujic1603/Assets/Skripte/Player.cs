8a13fb8d36c7008820a319b74fe9649ed6d8be01
MarkoPujic <79627058+MarkoPujic@users.noreply.github.com>
2021-03-16 16:33:25 +0100

    sdaf

---
+++ MarkoPujic1603/Assets/Skripte/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+            animator.SetInteger("HodanjeGore", 1);
+        }
+        else if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)))
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+            animator.SetInteger("HodanjeDolje", 1);
+        }
+        else
+        {
+            animator.SetInteger("HodanjeGore", 0);
+            animator.SetInteger("HodanjeDolje", 0);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+            animator.SetInteger("HodanjeLijevo", 1);
+
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+            animator.SetInteger("HodanjeDesno", 1);
+
+        }
+        else
+        {
+            animator.SetInteger("HodanjeLijevo", 0);
+            animator.SetInteger("HodanjeDesno", 0);
+        }
+    }
+}
---
+++ MarkoPujic2 3/Assets/Skripte/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    public float speedJump = 400f;
+
+    public bool jump = false;
+    public bool isGrounded = false;
+
+    Vector2 maxVelocity = new Vector2(5, 5);
+
+    public Animator animator;
+    public Rigidbody2D rb;
+    public SpriteRenderer _spriteRenderer;
+
+    public Transform GroundCheck, GroundCheckL, GroundCheckR;
+    public LayerMask groundLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!jump && isGrounded)
+            {
+                jump = true;
+            }
+        }
+        Debug.DrawLine(transform.position, GroundCheck.position, Color.red);
+        Debug.DrawLine(transform.position, GroundCheckL.position, Color.blue);
+        Debug.DrawLine(transform.position, GroundCheckR.position, Color.yellow);
+    }
+
+    private void FixedUpdate()
+    {
+        float forceX = 0f;
+        float forceY = 0f;
+
+        if (jump)
+        {
+            forceY = speedJump;
+        }
+        if (Physics2D.Linecast(transform.position, GroundCheck.position, groundLayer) ||
+            Physics2D.Linecast(transform.position, GroundCheckL.position, groundLayer) ||
+            Physics2D.Linecast(transform.position, GroundCheckR.position, groundLayer))
+        {
+            isGrounded = true;
+            jump = false;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        Vector2 v2 = new Vector2(forceX, forceY);
+        rb.AddForce(v2);
+    }
+}
